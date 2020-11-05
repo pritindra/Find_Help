a1ea00bd73f3fdc13247190b32f327e6bf1fe345
Pritindra <37242669+pritindra@users.noreply.github.com>
2020-11-05 21:58:07 +0530

    Add files via upload

--- interface/views.py
+++ interface/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Assistance, CanAssist
+from django_email_verification import sendConfirm
 
 from rest_framework import viewsets
 from rest_framework import permissions
@@ -30,11 +31,31 @@ def assistance(request):
         entry = Assistance.objects.create(email=email,name=name,addr1=address,addr2=address2,city=city,
         state=state,pin=pin,phone_number=phone_number,assistance_needed=assistance_needed,message=message)
         entry.save()
-
+        sendConfirm(entry)
+        
         return redirect('success')
     return render(request, 'interface/assistance.html')
 
 def can_assist(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        name = request.POST['name']
+        ngo_name = request.POST['ngo_name']
+        addr = request.POST['address']
+        city = request.POST['city']
+        state = request.POST['state']
+        pin = request.POST['pin']
+        phone_number = request.POST['phone_number']
+        assistance_offer = request.POST['assistance_offer']
+        message = request.POST['message']
+
+    # Saving entries
+        entry = CanAssist.objects.create(email=email,name=name,ngo_name=ngo_name,addr=address,city=city,
+        state=state,pin=pin,phone_number=phone_number,assistance_offer=assistance_offer,message=message)
+        entry.save()
+        sendConfirm(entry)
+        
+        return redirect('success')
     return render(request, 'interface/can_assist.html')
 
 class AssistanceViewSet(viewsets.ModelViewSet):
